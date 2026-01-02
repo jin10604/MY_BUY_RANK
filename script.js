4a009503d1109bf782ef97df016fbbf70c030aff
jin10604 <jinco0428@naver.com>
2026-01-02 19:23:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const modal = document.getElementById("modal");
-document.getElementById("openModal").onclick = () => modal.classList.add("show");
-document.getElementById("closeModal").onclick = () => modal.classList.remove("show");
+const openBtn = document.getElementById("openModal");
+const cancelBtn = document.getElementById("cancelBtn");
+const addBtn = document.getElementById("addBtn");
+const itemList = document.getElementById("itemList");
 
-document.getElementById("addItem").onclick = () => modal.classList.remove("show");
+openBtn.onclick = () => modal.classList.remove("hidden");
+cancelBtn.onclick = () => modal.classList.add("hidden");
+
+addBtn.onclick = () => {
+  const name = document.getElementById("itemName").value;
+  const price = document.getElementById("itemPrice").value;
+  const memo = document.getElementById("itemMemo").value;
+  const file = document.getElementById("itemImage").files[0];
+
+  if(!name) return;
+
+  const item = document.createElement("div");
+  item.className="item";
+
+  if(file){
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    item.appendChild(img);
+  }
+
+  const txt = document.createElement("p");
+  txt.innerText = `${name}\n${price}\n${memo}`;
+  item.appendChild(txt);
+
+  itemList.appendChild(item);
+  modal.classList.add("hidden");
+};
